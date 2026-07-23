4416fd0be25013acb9bcae1599939cfe9b6402ec
João Gabriel <joaogab88hotmal@gmail.com>
2026-07-23 20:57:24 -0300

    Subtituindo as variáveis sensiveis por env, para adicionar uma camada de segurança

--- src/config.py
+++ src/config.py
@@ -1,10 +1,11 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "sistema-licenca-dev")
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "mysql+pymysql://root:Jgpds28%25@localhost/sistema_licenca_capacitacao",
-    )
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
